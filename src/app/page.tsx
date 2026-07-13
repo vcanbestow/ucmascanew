1ae514a8f1e84f421708c053681c8891cfb5bc8d
BestowVcan <maintenance.vcan@gmail.com>
2026-07-13 13:06:42 +0530

    server vcan

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
 
       </div>
       <ParentsWorryAbout />
-      <CognitiveCalculator />
+      <CognitiveCalculator /> 
       <FlagshipPrograms />
       <CtaSection />
       <UpcomingWorkshops />
